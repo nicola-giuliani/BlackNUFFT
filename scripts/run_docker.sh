f6040953ba24794d5f5fcab0bc023a35316e335d
Nicola Giuliani <ngiuliani@sissa.it>
2017-06-12 13:48:30 +0200

    running test locally

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,4 +4,6 @@ echo $IMG
 
 CMD=`grep -e '- ' .gitlab-ci.yml | sed 's/- //'`
 
-docker run -v `pwd`:/builds/luca-heltai/dealii-bare-app $IMG /bin/sh -c "$CMD" 
+
+docker run -v `pwd`:/builds/blcknufft/ -i -t mathlab/deal2lkit:v8.5.0-debugrelease bash
+#docker run -v `pwd`:/builds/luca-heltai/dealii-bare-app $IMG /bin/sh -c "$CMD" 
