75312217c82e06f2c69a35795f9c8bf46ab01db2
nicola.giuliani-CMAP <nicola.giuliani@hpc.cm.cluster>
2017-06-13 14:19:21 +0200

    ahio

---
+++ build_tbb_2/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/source/main.cc.o"
+  "CMakeFiles/demo.dir/source/black_nufft.cc.o"
+  "demo.pdb"
+  "demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
